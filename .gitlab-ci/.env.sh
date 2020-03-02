12976b1a86812885697c1c8552270a85d2650267
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-03-02 15:26:14 +0100

    test again, and again

---
+++ .gitlab-ci/.env.sh
@@ -0,0 +1,7 @@
+export BRANCH_NAME=${BRANCH_NAME:=$CI_COMMIT_REF_SLUG}
+export HASH_SIZE=${HASH_SIZE:=7}
+
+BRANCH_NAME_HASHED=$( printf "${BRANCH_NAME}" | sha1sum | cut -c1-${HASH_SIZE} )
+export BRANCH_HASH=${BRANCH_HASH:="$BRANCH_NAME_HASHED"}
+
+export K8S_NAMESPACE="hasura-feature-${BRANCH_HASH}"
